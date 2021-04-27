acbdfd235e6218a663de5f2834fd0d24655d1a46
FamRau <gerhard.rauniak@gmail.com>
2021-04-27 20:06:33 +0200

    added basic register form and success.html, failure.html

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+SPORTS = ["Running", "Soccer", "Basketball", "Swimming"]
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html", sports=SPORTS)
+    if request.method == "POST":
+        if request.form.get("sport") not in SPORTS:
+            return render_template("failure.html")
+        else:    
+            return render_template("success.html")
+
+
