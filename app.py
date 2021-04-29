1585145daef8b9ddcb3c01a4ffc715242d097b05
FamRau <gerhard.rauniak@gmail.com>
2021-04-29 17:13:08 +0200

    added error.html and registrants.html

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+REGISTRANTS  = {}
 SPORTS = ["Running", "Soccer", "Basketball", "Swimming"]
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/")
 def index():
-    if request.method == "GET":
-        return render_template("index.html", sports=SPORTS)
-    if request.method == "POST":
-        if request.form.get("sport") not in SPORTS:
-            return render_template("failure.html")
-        else:    
-            return render_template("success.html")
+    return render_template("index.html", sports=SPORTS)
 
 
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Invalid sport")
+
+    REGISTRANTS[name] = sport
+    return render_template("registrants.html", registrants=REGISTRANTS)
