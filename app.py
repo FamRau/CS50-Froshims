355f30824e68956a3b064c714864853b243d4bb1
FamRau <gerhard.rauniak@gmail.com>
2021-04-30 21:03:21 +0200

    sone tweaks to registrants view

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
+from flask_mail import Mail, Message
+from flask_session import Session
 import sqlite3 as sql
+import os
 app = Flask(__name__)
 
+
+
 SPORTS = ["Running", "Soccer", "Basketball", "Swimming"]
+
 
 
 @app.route("/")
@@ -12,9 +18,9 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
+    email = request.form.get("email")
+    if not email:
+        return render_template("error.html", message="Missing email")
     sport = request.form.get("sport")
     if not sport:
         return render_template("error.html", message="Missing sport")
@@ -24,16 +30,23 @@ def register():
     with sql.connect("froshims.db") as con:
         cur = con.cursor()
         cur.execute(
-            "INSERT INTO registrants (name, sport) VALUES (?, ?)", (name, sport))
+            "INSERT INTO registrants (name, sport) VALUES (?, ?)", (email, sport))
         con.commit()
-        return render_template("success.html", message="Succesfully registered")
+  
     return redirect("/registrants")
 
 
 @app.route("/registrants")
 def registrants():
-    with sql.connect("froshims.db") as con:
-        cur = con.cursor()
-        registrants = cur.execute("select * FROM registrants")
-    return render_template("registrants.html", registrants=registrants)
-    close(con)
+    # with sql.connect("froshims.db") as con:
+    #     cur = con.cursor()
+    #     rows = cur.execute("select * FROM registrants")
+    #     return render_template("registrants.html", rows=rows)
+    con = sql.connect("froshims.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM registrants")
+
+    rows = cur.fetchall()
+    return render_template("registrants.html", rows = rows)
